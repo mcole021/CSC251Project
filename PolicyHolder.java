873214eb636357e32cebf697610220a042df66b7
mcole021 <mcole021@email.cpcc.edu>
2023-06-28 23:01:20 -0400

    Update PolicyHolder.java
    Added toString method

--- PolicyHolder.java
+++ PolicyHolder.java
@@ -122,4 +122,18 @@ public class PolicyHolder
       
       return (weight * BMIF) / (height * height);
    }
+   
+   /**
+   @return A String that describes the PolicyHolder
+   */
+   public String toString()
+   {
+      return String.format("Policyholder's First Name: " + firstName + 
+                           "\nPolicyholder's Last Name: " + lastName + 
+                           "\nPolicyholder's Age: " + age +
+                           "\nPolicyholder's Smoking Status: " + smokingStatus +
+                           "\nPolicyholder's Height: " + height + " inches" +
+                           "\nPolicyholder's Weight: " + weight + " pounds" +
+                           "\nPolicyholder's BMI: %.2f" , getBMI());
+   }
 }
